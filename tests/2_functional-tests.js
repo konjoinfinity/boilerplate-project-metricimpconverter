9142c67b4d45dbc81679a34f8714adbf7a0c5c66
Wesley Scholl <clients@konjoweb.com>
2023-02-26 19:03:19 -0500

    all tests pass, unit and functional - 16

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,12 +7,85 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
    
+        suite("GET request at /api/convert to convert", () => {
+          test("10L Conversion to Gallons (Valid)",(done) => {
+            chai
+              .request(server)
+              .get("/api/convert")
+              .query({ input: "10L" })
+              .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.initNum, 10);
+                assert.equal(res.body.initUnit, "L");
+                assert.equal(res.body.returnNum, 2.64, 0.1);
+                assert.equal(res.body.returnUnit, "gal");
+                done();
+              });
+            })
+        })
+          
+        suite("GET request for invalid unit input", () => {
+            test("32g (Invalid) to return invalid",(done) => {
+              chai
+                .request(server)
+                .get("/api/convert")
+                .query({ input: "32g" })
+                .end((err, res) => {
+                  assert.equal(res.status, 200);
+                  assert.equal(res.body.initUnit, undefined);
+                  done();
+                });
+              })
+          })
+
+          suite("GET request for invalid fraction input", () => {
+            test("3/7.2/4kg (Invalid) to return invalid",(done) => {
+              chai
+                .request(server)
+                .get("/api/convert")
+                .query({ input: "3/7.2/4kg" })
+                .end((err, res) => {
+                  assert.equal(res.status, 200);
+                  assert.equal(res.body.initUnit, undefined);
+                  done();
+                });
+              })
+          })
+
+          suite("GET request for invalid fraction and unit", () => {
+            test("3/7.2/4kilomegagram (Invalid) to return invalid",(done) => {
+              chai
+                .request(server)
+                .get("/api/convert")
+                .query({ input: "3/7.2/4kilomegagram" })
+                .end((err, res) => {
+                  assert.equal(res.status, 200);
+                  assert.equal(res.body.initUnit, undefined);
+                  done();
+                });
+              })
+          })
+
+          suite("GET request at /api/convert to convert with no number", () => {
+            test("[No number]kg Conversion to Gallons (Valid)",(done) => {
+              chai
+                .request(server)
+                .get("/api/convert")
+                .query({ input: "kg" })
+                .end((err, res) => {
+                  assert.equal(res.status, 200);
+                  assert.equal(res.body.initNum, 1);
+                  assert.equal(res.body.initUnit, "kg");
+                  assert.equal(res.body.returnNum, 2.2, 0.1);
+                  assert.equal(res.body.returnUnit, "lbs");
+                  done();
+                });
+              })
+          })
+
+
 
 });
 
 
-// Convert a valid input such as 10L: GET request to /api/convert.
-// Convert an invalid input such as 32g: GET request to /api/convert.
-// Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.
-// Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.
 // Convert with no number such as kg: GET request to /api/convert.
